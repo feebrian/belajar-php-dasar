148fb32e0ac78ea00744893b7b9dc7ae77ab9dfc
feebrian <adamaditama13@gmail.com>
2022-12-25 06:55:38 +0700

    yeahhh finally i can make this file rename on my own

--- implode.php
+++ implode.php
@@ -1,6 +1,35 @@
-<html>
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+
+  $name = "Adam Febrian Aditama";
+  $explode = explode(" ", $name);
+  $result = implode("-", $explode);
+
+  echo nl2br($result . "\n");
+
+
+  // testing
+  $fileName = "asdf.jpg";
+  $fileProp = explode(".", $fileName);
+  $ext = end($fileProp);
+
+  $randomNum = (array)random_int(12345, 98765);
+  $newFileName = implode(".", $randomNum) . "." . $ext;
+
+  echo $newFileName;
+
+  ?>
+
+</body>
 
 </html>
