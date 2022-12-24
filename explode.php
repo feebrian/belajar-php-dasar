bb00d3d130fec8f1d20091560633102e5406bae1
feebrian <adamaditama13@gmail.com>
2022-12-24 20:54:29 +0700

    i can use explode a lil bit

--- explode.php
+++ explode.php
@@ -1,6 +1,19 @@
 <html>
 <?php
 
+// percobaan explode function
+$asdf = "Adam Febrian Aditama";
+$x = explode(" ", $asdf);
+$length = strlen($asdf);
+
+for ($i = 0; $i < $length; $i++) {
+  echo "<pre>";
+  echo $x[$i];
+  echo "</pre>";
+}
+
+// 
+
 ?>
 
 </html>
