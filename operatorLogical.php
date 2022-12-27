942a1034da7182c1b1bbc0978825954b40d9b42c
feebrian <adamaditama13@gmail.com>
2022-12-27 16:07:08 +0700

    learned logical operator

---
+++ operatorLogical.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Logical Operator</title>
+</head>
+
+<body>
+  <?php
+  include("myFunction.php");
+  $a = true || false;
+  $b = true || true;
+  var_dump($a xor $b);
+  breakln();
+  var_dump($a && $b);
+  ?>
+</body>
+
+</html>
