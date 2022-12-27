4974913447ec3f7fbef7401aefddd07730246cf9
feebrian <adamaditama13@gmail.com>
2022-12-27 16:33:11 +0700

    learned conditional operator

--- operatorConditional.php
+++ operatorConditional.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Conditional Operator</title>
 </head>
+
 <body>
-  
+  <?php
+  include("myFunction.php");
+  $num1 = 11;
+  $num = $num1 < 10 ? $num1 : "Angka melebihi 10";
+
+  echo $num;
+
+  // null coalescing
+  breakln();
+  $num3 = 12;
+  $num = $num3 ?? "Adalah null";
+  echo $num;
+  ?>
 </body>
+
 </html>
