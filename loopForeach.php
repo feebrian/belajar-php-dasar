6cd0bbe5d764a9541594e13b73862414a6d4ef13
feebrian <adamaditama13@gmail.com>
2022-12-28 16:11:29 +0700

    learning foreach loop and a bit of array

--- loopForeach.php
+++ loopForeach.php
@@ -1,7 +1,22 @@
 <?php
 
-$num = array(1, 2, 3, 4);
+// associative array
+$people = array('name1' => 'Adam', 'name2' => 'Febrian', 'name3' => 'Aditama');
 
-foreach ($num as &$y) {
-  echo $y * 2 . "\n";
+foreach ($people as $key => $value) {
+  echo "$key = $value \n";
+}
+
+// multidimensional array
+$people =
+  array(
+    'name' => 'adam'
+  );
+
+for ($i = 1, $size = count($people); $i < $size; $i++) {
+  print_r($people[$i]);
+}
+
+foreach ($people as $key => $value) {
+  echo "$key = $value \n";
 }
