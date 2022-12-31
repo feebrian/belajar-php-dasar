f7e9b0828e518ded9b2de154802d9d71e15777c7
feebrian <adamaditama13@gmail.com>
2022-12-31 16:00:10 +0700

    learn array

--- array.php
+++ array.php
@@ -1,14 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<body>
-  <?php
+// indexed array
+$cars = array("BMW", "Avanza", "Pajero");
 
-  // array
-  $cars = array("BMW", "Avanza", "Pajero");
-  var_dump($cars);
+foreach ($cars as $car) {
+  echo "I like $car" . PHP_EOL;
+}
 
-  ?>
-</body>
+// associative array
+$peoples = array('Adam' => 'SMK 1', 'Azzam' => 'SMA 1', 'Yanuar' => 'Muhammadiyah 1');
+foreach ($peoples as $name => $school) {
+  echo "$name bersekolah di $school" . PHP_EOL;
+}
 
-</html>
+// multidimensional array
+$weather = [
+  array('January', 'Rainy'),
+  array('February', 'Rainy'),
+  array('March', 'Dry'),
+  array('April', 'Dry')
+];
+
+// echo "Weather in " . $weather[0][0] . " is " . $weather[0][1] . PHP_EOL;
+for ($w = 0; $w < 3; $w++) {
+  echo "Weather in " . $weather[$w][0] . " is " . $weather[$w][1] . PHP_EOL;
+}
+
+// range on array
+foreach (range(0, 100, 10) as $num) {
+  echo $num;
+}
