177bf333e3e2ea16d6d189e5e6d802be01aabb7a
feebrian <adamaditama13@gmail.com>
2023-01-01 16:13:17 +0700

    learn how to connect php to mysql oop and procedural technique

--- databaseMysql.php
+++ databaseMysql.php
@@ -1,1 +1,29 @@
 <?php
+
+// object oriented
+$hostname = "localhost";
+$username = "root";
+$password = "adamfebrian";
+
+$conn = new mysqli($hostname, $username, $password);
+
+// check connection
+if (!$conn) {
+  die("Connection failed" . $conn->connect_error);
+} else {
+  echo "Connection Success" . PHP_EOL;
+}
+
+
+// procedural
+$conn = mysqli_connect($hostname, $username, $password);
+
+if (!$conn) {
+  die("Connection failed" . mysqli_connect_error());
+} else {
+  echo "Connection Success" . PHP_EOL;
+}
+
+// closing mysql connection manually
+$conn->close(); // oop
+mysqli_close($conn); // procedural
