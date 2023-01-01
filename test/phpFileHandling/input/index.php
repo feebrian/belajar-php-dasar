1f707607ddd7a0c5b19e21a0a18775f70074a56e
feebrian <adamaditama13@gmail.com>
2023-01-01 19:25:42 +0700

    move file to /input

---
+++ test/phpFileHandling/input/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>File Handling</title>
+</head>
+<body>
+  <form action="indexProcess.php" method="post">
+    <input type="text" name="name">
+    <input type="submit" name="submit" value="Submit">
+  </form>
+</body>
+</html>
