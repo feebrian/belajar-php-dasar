70308d696593d27100b7a86b25b3be88bb0d43f7
feebrian <adamaditama13@gmail.com>
2023-01-02 21:58:35 +0700

    learn php file handling

--- test/phpFileHandling/openfile.php
+++ test/phpFileHandling/openfile.php
@@ -1,1 +1,14 @@
 <?php
+
+if (file_exists("index.html")) {
+
+  $file = "index.html";
+  $file_open = fopen($file, "a"); // open file
+  $file_size = filesize($file); // get file size
+  $data = fwrite($file_open, "This is a string"); // assign file data to $data
+
+  var_dump($data);
+
+} else {
+  echo "File not found";
+}
